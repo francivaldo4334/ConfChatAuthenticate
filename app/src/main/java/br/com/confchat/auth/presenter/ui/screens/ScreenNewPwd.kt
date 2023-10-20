7aee21abae863f9090c8e38aa4c4184ca6e624e5
unknown <>
2023-10-20 20:56:10 -0300

    feat: screen new pwd

---
+++ app/src/main/java/br/com/confchat/auth/presenter/ui/screens/ScreenNewPwd.kt
@@ -0,0 +1,84 @@
+package br.com.confchat.auth.presenter.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.confchat.auth.presenter.ui.components.ComponentDropUpContainer
+import br.com.confchat.auth.presenter.ui.components.ComponentTextFieldOutline
+import br.com.confchat.auth.presenter.ui.components.ComponentTextFieldOutlinePwd
+
+@Composable
+fun ScreenNewPwd(open:Boolean, onDismiss:()->Unit) {
+    var username by remember {
+        mutableStateOf("")
+    }
+    var pwd by remember {
+        mutableStateOf("")
+    }
+    var comfirPwd by remember {
+        mutableStateOf("")
+    }
+    ComponentDropUpContainer(expanded = open, onDimiss = onDismiss) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Adicionar dados de autenticacao")
+                IconButton(onClick = onDismiss) {
+                    Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                }
+            }
+            ComponentTextFieldOutline(
+                label = "Username",
+                value = username,
+                onValue = {username = it},
+                modifier = Modifier.fillMaxWidth()
+            )
+            ComponentTextFieldOutlinePwd(
+                label = "Password",
+                value = pwd,
+                onValue = {pwd = it},
+                modifier = Modifier.fillMaxWidth()
+            )
+            ComponentTextFieldOutlinePwd(
+                label = "Comfir password",
+                value = comfirPwd,
+                onValue = {comfirPwd = it},
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(56.dp))
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScreenNewPwdPreviw() {
+    ScreenNewPwd(true){}
+}
